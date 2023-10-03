a4803c7050190d5eea3d8e2851eb242fe3084f38
theEricB <46425157+theEricB@users.noreply.github.com>
2023-10-03 20:35:59 +0200

    Update index.php

--- rezepte/index.php
+++ rezepte/index.php
@@ -3,6 +3,8 @@
 <head>
     <title>CowFoot Rezepte</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 <body>
     <h1>CowFoot Rezepte</h1>
