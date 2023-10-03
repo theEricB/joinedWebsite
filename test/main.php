c2e480b0d561e0b02af065012d4d5a8037432bed
theEricB <46425157+theEricB@users.noreply.github.com>
2023-10-03 19:16:01 +0200

    Update main.php

--- test/main.php
+++ test/main.php
@@ -13,6 +13,7 @@
     $username = "u260926282_recipes"; // Change this to your database username
     $password = "o~Z0?:GG:"; // Change this to your database password
     $database = "u260926282_recipes"; // Change this to your database name
+    
     $conn = new mysqli($host, $username, $password, $database);
 
     if ($conn->connect_error) {
@@ -24,7 +25,11 @@
         $delete_sql = "DELETE FROM recipes WHERE id = ?";
         $stmt = $conn->prepare($delete_sql);
         $stmt->bind_param("i", $delete_id);
-        $stmt->execute();
+        if ($stmt->execute()) {
+            echo "Entry deleted successfully.";
+        } else {
+            echo "Error deleting entry: " . $stmt->error;
+        }
         $stmt->close();
     }
 
@@ -38,7 +43,8 @@
             echo "<p><strong>Date:</strong> {$row['date']}</p>";
             echo "<p><strong>Ingredients:</strong><br>{$row['ingredients']}</p>";
             echo "<p><strong>Description:</strong><br>{$row['description']}</p>";
-            echo "<form method='post' action='main.php?delete_id={$row['id']}'>";
+            echo "<form method='post' action='main.php'>";
+            echo "<input type='hidden' name='delete_id' value='{$row['id']}'>";
             echo "<input type='submit' value='Delete'>";
             echo "</form>";
             echo "<hr>";
