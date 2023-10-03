4be2aa49b3c97a20622afa50bc941736d8e4bb03
theEricB <46425157+theEricB@users.noreply.github.com>
2023-10-03 19:19:32 +0200

    update

--- test/index.php
+++ test/index.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Recipe Entry</title>
+    <title>Neues Rezept</title>
 </head>
 <body>
-    <h1>Recipe Entry</h1>
+    <h1>Neues Rezept:</h1>
     
     <form method="post" action="save.php">
-        <label for="name">Name:</label>
+        <label for="name">Titel:</label>
         <input type="text" name="name" id="name" required><br><br>
 
-        <label for="ingredients">Ingredients:</label><br>
+        <label for="ingredients">Zutaten:</label><br>
         <textarea name="ingredients" id="ingredients" rows="4" cols="50" required></textarea><br><br>
 
-        <label for="description">Description:</label><br>
+        <label for="description">Beschreibung:</label><br>
         <textarea name="description" id="description" rows="4" cols="50" required></textarea><br><br>
 
         <label for="author">Author:</label>
         <input type="text" name="author" id="author" required><br><br>
 
-        <label for="date">Date:</label>
+        <label for="date">Datum:</label>
         <input type="date" name="date" id="date" required><br><br>
 
         <button type="submit">Save</button>
     </form>
     
     <br><br>
-    <a href="main.php">View All Entries</a>
+    <a href="main.php">Zeige alle Rezepte</a>
 </body>
 </html>
--- test/main.php
+++ test/main.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Recipe Blog</title>
+    <title>CowFoot Rezepte</title>
 </head>
 <body>
-    <h1>Recipe Blog</h1>
+    <h1>CowFoot Rezepte</h1>
     
-    <a href="index.php">Make a New Entry</a><br><br>
+    <a href="index.php">Rezept hinzufügen...</a><br><br>
     
     <?php
     $host = "localhost"; // Change this to your database host
     $username = "u260926282_recipes"; // Change this to your database username
     $password = "o~Z0?:GG:"; // Change this to your database password
     $database = "u260926282_recipes"; // Change this to your database name
-    
     $conn = new mysqli($host, $username, $password, $database);
 
     if ($conn->connect_error) {
@@ -25,11 +24,7 @@
         $delete_sql = "DELETE FROM recipes WHERE id = ?";
         $stmt = $conn->prepare($delete_sql);
         $stmt->bind_param("i", $delete_id);
-        if ($stmt->execute()) {
-            echo "Entry deleted successfully.";
-        } else {
-            echo "Error deleting entry: " . $stmt->error;
-        }
+        $stmt->execute();
         $stmt->close();
     }
 
@@ -43,8 +38,7 @@
             echo "<p><strong>Date:</strong> {$row['date']}</p>";
             echo "<p><strong>Ingredients:</strong><br>{$row['ingredients']}</p>";
             echo "<p><strong>Description:</strong><br>{$row['description']}</p>";
-            echo "<form method='post' action='main.php'>";
-            echo "<input type='hidden' name='delete_id' value='{$row['id']}'>";
+            echo "<form method='post' action='main.php?delete_id={$row['id']}'>";
             echo "<input type='submit' value='Delete'>";
             echo "</form>";
             echo "<hr>";
