494e23df6e4d64024ba3cee728a25a347a067456
theEricB <46425157+theEricB@users.noreply.github.com>
2023-10-03 18:56:39 +0200

    test

---
+++ recipes/recipe.js
@@ -0,0 +1,2 @@
+const nam = "erik";
+console.log(nam);
---
+++ test/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Save Input to Database</title>
+</head>
+<body>
+    <h1>Save Input to Database</h1>
+    
+    <form method="post" action="save.php">
+        <label for="input_value">Enter a value:</label>
+        <input type="text" name="input_value" id="input_value" required>
+        <button type="submit">Save</button>
+    </form>
+</body>
+</html>
