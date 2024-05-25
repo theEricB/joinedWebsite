fde943eb152cc0b774d7ee9318133a64be869839
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-05-25 12:26:31 +0200

    sorry now

--- angi2.0/countdown.js
+++ angi2.0/countdown.js
@@ -25,7 +25,7 @@ function updateCountdown() {
     countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
     // Update image based on the remaining days
-    updateImage(days, seconds);
+    updateImage(days, hours);
 }
 
 function updateImage(days, hours) {
