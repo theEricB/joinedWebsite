da33df4d3b8f227800b5367b1faa87d094be1008
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-05-25 12:17:31 +0200

    test222

--- angi2.0/countdown.js
+++ angi2.0/countdown.js
@@ -29,7 +29,11 @@ function updateCountdown() {
 }
 
 function updateImage(days, hours) {
-    const cacheBuster = new Date().getTime(); // Unique timestamp to prevent caching
+    const cacheBuster = new Date().getTime();
+     // Unique timestamp to prevent caching
+     if (Math.floor(hours / 12) > 2){
+        hours = 24
+     }
     const imagePath = `images/${days * 10 + Math.floor((hours / 12) + 1)}.jpg?cb=${cacheBuster}`; // Assumes images are named like "9.jpg", "8.jpg", etc.
     console.log(imagePath)
     countdownImage.src = imagePath;
