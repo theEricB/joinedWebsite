9298a696614ae32fe4206be5ec92666fa0aad829
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-06-03 19:11:34 +0200

    dsncks

--- angi2.0/countdown.js
+++ angi2.0/countdown.js
@@ -5,25 +5,36 @@ const targetDate = new Date("June 3, 2024 22:00:00").getTime();
 console.log("Script geladen, Zielzeit: ", targetDate);
 
 const countdownElement = document.getElementById('countdown');
+const textElemenent = document.getElementById('sorry');
 const countdownImage = document.getElementById('countdown-image');
 
 function updateCountdown() {
     const now = new Date().getTime();
     const timeLeft = targetDate - now;
 
-    if (timeLeft < 0) {
-        countdownElement.innerHTML = "Klopf Klopf!";
-        countdownImage.style.display = 'none';
-        return;
-    }
 
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    countdownElement.innerHTML = `${hours}h ${minutes}m ${seconds}s`;
-    //countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    if (timeLeft < 0) {
+        countdownElement.innerHTML = "Klopf Klopf!";
+        countdownElement.style.top = "40%";
+        textElemenent.innerHTML = "";
+        countdownImage.src = `images/0.jpg?cb=3.3`;
+        countdownImage.style.display = 'block';
+        return;
+    } else if (days < 1 && hours < 1 && minutes < 1) {
+        countdownElement.innerHTML = `${seconds}s`;
+    } else if (days < 1 && hours < 1) {
+        countdownElement.innerHTML = `${minutes}m ${seconds}s`;
+    } else if (days < 1) {
+        countdownElement.innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+    } else {
+        countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+    
 
     // Update image based on the remaining days
     updateImage(days, hours);
@@ -31,7 +42,7 @@ function updateCountdown() {
 
 function updateImage(days, hours) {
      // Unique timestamp to prevent caching
-    const imagePath = `images/${days * 10 + Math.floor((hours / 12) + 1)}.jpg?cb=1.2`;
+    const imagePath = `images/${days * 10 + Math.floor((hours / 12) + 1)}.jpg?cb=1.3`;
     countdownImage.src = imagePath;
     countdownImage.style.display = 'block';
 }
@@ -41,3 +52,7 @@ setInterval(updateCountdown, 1000);
 
 // Initial call to display the countdown immediately
 updateCountdown();
+
+function add10Min() {
+    targetDate = targetDate + (10 * 60 * 1000);
+}
