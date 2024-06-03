623136569d77d5f869cec4a9d59b6ef6eb3a69e3
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-06-03 18:33:30 +0200

    ndkckdn

--- angi2.0/countdown.js
+++ angi2.0/countdown.js
@@ -22,7 +22,8 @@ function updateCountdown() {
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    countdownElement.innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+    //countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
     // Update image based on the remaining days
     updateImage(days, hours);
